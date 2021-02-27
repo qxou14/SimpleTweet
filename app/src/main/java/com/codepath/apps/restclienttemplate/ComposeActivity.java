3383f5dfce0dda7f13855782183e4e471297f609
qxou14 <qxou14@gmail.com>
2021-02-27 00:31:26 -0500

    4th commit

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -2,12 +2,17 @@ package com.codepath.apps.restclienttemplate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
@@ -23,8 +28,10 @@ public class ComposeActivity extends AppCompatActivity {
     public static final String TAG = "ComposeActivity";
     EditText etCompose;
     Button btnTweet;
-    private static final int WORD_LIMIT = 140;
+    private static final int WORD_LIMIT = 280;
     TwitterClient client;
+
+    TextView tvWordcount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,8 +40,42 @@ public class ComposeActivity extends AppCompatActivity {
 
         etCompose = findViewById(R.id.etCompose);
         btnTweet = findViewById(R.id.btnTweet);
+        tvWordcount = findViewById(R.id.tvWordcount);
         client = TwitterApp.getRestClient(this);
 
+        etCompose.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // Fires right as the text is being changed (even supplies the range of text)
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count,
+                                          int after) {
+                // Fires right before text is changing
+            }
+
+            @SuppressLint({"SetTextI18n"})
+            @Override
+            public void afterTextChanged(Editable s) {
+
+                int word_len = s.length();
+
+                if(word_len >WORD_LIMIT)
+                {
+                    tvWordcount.setTextColor(Color.parseColor("#FF0000"));
+                }
+                else
+                {
+                    tvWordcount.setTextColor(Color.parseColor("#000000"));
+
+
+                }
+
+                tvWordcount.setText(String.valueOf(word_len) + "/" + String.valueOf(WORD_LIMIT));
+
+            }
+        });
         btnTweet.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
